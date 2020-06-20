2870483913d81ad487c2817210a05321db5e2116
Pankaj Kumar <pankajk@orowealth.com>
2020-06-20 22:09:56 +0530

    Implemented Login Using Google passport strategy

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const path = require("path")
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 
 const connectDB = require("./config/db");
 
 //Load config
 dotenv.config({ path: "./config/config.env" });
+
+//Passport config
+require("./config/passport")(passport);
 
 //Connect DB
 connectDB();
@@ -20,13 +25,34 @@ if (process.env.NODE_ENV === "development") {
 }
 
 //Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs", layoutsDir:"./views/layout" }));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: "./views/layout",
+  })
+);
 app.set("view engine", ".hbs");
 
+//Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Static folder
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 //Routes
-app.use("/", require("./routes/index"))
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
